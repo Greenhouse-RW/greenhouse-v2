dea137e7f9a0a3dd5bbc548d9662061f2d130356
Tracy <tracymurenzi@gmail.com>
2021-05-03 13:45:12 +0200

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import * as FaIcons from "react-icons/fa";
 
 function Navbar() {
   return (
     <div>
-      
+      <div className="navbar">
+        <Link to="#" className='menu-bars'>
+          <FaIcons.FaBars/>
+        </Link>
+      </div>
     </div>
   )
 }
