1c9930399f27e4e2e74948119c138ee9290e4722
Tracy <tracymurenzi@gmail.com>
2021-05-03 14:34:14 +0200

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
+      <IconContext.Provider value={{ color: '#000' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
