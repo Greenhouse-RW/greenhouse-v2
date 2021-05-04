f90710c58657c3ca481797775258aea174989a5d
Tracy <tracymurenzi@gmail.com>
2021-05-04 12:09:00 +0200

    init

--- src/components/Calendar/SidebarCalendar.js
+++ src/components/Calendar/SidebarCalendar.js
@@ -1,11 +1,18 @@
-
 import React from 'react'
 import "./SidebarCalendar.css";
 
-export const SidebarCalendar = () => {
+export default function SidebarCalendar() {
     return (
         <div>
-            <h2>Tracy</h2>
+            <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  It has survived not only five centuries, but also the leap into electronic typesetting,
+                   remaining essentially unchanged. It was popularised in the 1960s with the release of
+                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+                    software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
         </div>
     )
 }
+
+// export default SidebarCalendar;
