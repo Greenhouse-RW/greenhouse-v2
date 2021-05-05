d7f2025eb18c8ea3ef77f543620b4da38eec96bc
Tracy <tracymurenzi@gmail.com>
2021-05-05 15:34:05 +0200

    init

---
+++ src/components/ControlFacilities/Control.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export const Control = () => {
+    return (
+        <div className="MySideCale h-screen pt-20 fixed">
+      <h1 className="recorded text-2xl text-white">Recorded Values.</h1>
+
+      <ul className="sidebardata">
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+    
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+
+        <li className="flex  text-white text-lg">
+          {" "}
+          <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+ <p>&#176;C</p>{" "}
+        </li>
+
+        
+       
+      </ul>
+    </div>
+    )
+}
