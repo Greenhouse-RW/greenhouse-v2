67a55c20caa87d2a6b14902a38f80c0fa2362aee
Tracy <tracymurenzi@gmail.com>
2021-05-12 10:23:39 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -2,52 +2,72 @@ import React from 'react'
 
 export const Data = () => {
     return (
-        <div className=" h-screen pl-60 pt-20 fixed">
-        <h1 className=" text-2xl text-black font-bold">ENVIRONMENT DATA.</h1>
-  
-        <ul className="pt-10 pl-3">
-          <li className="flex  text-black text-lg">
+        <div className=" h-screen pt-24 fixed overflow-auto">
+        <h1 className="recorded text-xl text-black text-center">
+          Recorded Values
+        </h1>
+
+        <ul className="flex flex-col justify-center items-center">
+
+        <li className="flex  text-black text-sm">
             {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
+            <li className="pb-6 mt-2">Humidity</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
           </li>
-      
-          <li className="flex  text-black text-lg">
+
+          <li className="flex  text-black text-sm">
             {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
+            <li className="pb-6 mt-2">Illumination</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
           </li>
-  
-          <li className="flex  text-black text-lg">
+
+          <li className="flex  text-black text-sm">
             {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
+            <li className="pb-6 mt-2">Temperature</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
           </li>
-  
-          <li className="flex  text-black text-lg">
-            {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
-          </li>
-          <li className="flex  text-black text-lg">
-            {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
-          </li>
-  
-          <li className="flex  text-black text-lg">
-            {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
-          </li>
-  
-          <li className="flex  text-black text-lg">
-            {" "}
-            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
-   <p>&#176;C</p>{" "}
-          </li>
-  
+
           
+          <li className="flex  text-black text-sm">
+            {" "}
+            <li className="pb-6 mt-2">Soil moisture</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
+          </li>
+
+
+          <li className="flex  text-black text-sm">
+            {" "}
+            <li className="pb-6 mt-2">Carbondioxide</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
+          </li>
+        
+          <li className="flex  text-black text-sm">
+            {" "}
+            <li className="pb-6 mt-2">Potential of H2</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
+          </li>
+
+
+          <li className="flex  text-black text-sm">
+            {" "}
+            <li className="pb-6 mt-2">Soil temperature</li>{" "}
+            <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
+            <p className="mt-2">&#176;C</p>{" "}
+          </li>
+        
          
         </ul>
       </div>
