e4ca39c17bf592678e3d9e890ef5aa795087f2c2
Tracy <tracymurenzi@gmail.com>
2021-05-12 10:28:06 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -3,13 +3,13 @@ import React from 'react'
 export const Data = () => {
     return (
         <div className="  pl-64 pt-20 fixed overflow-auto">
-        <h1 className="text-xl text-black text-center font-bold">
+        <h1 className="text-xl text-black text-center font-bold pb-6">
           ENVIORNMENT DATA.
         </h1>
 
         <ul className="flex flex-col justify-center items-center">
 
-        <li className="flex  text-black text-sm">
+        <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Humidity</li>{" "}
             <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
@@ -17,7 +17,7 @@ export const Data = () => {
             <p className="mt-2">&#176;C</p>{" "}
           </li>
 
-          <li className="flex  text-black text-sm">
+          <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Illumination</li>{" "}
             <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
