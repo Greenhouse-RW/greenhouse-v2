fcc37e6cc8a99d65c9fb8459f2ae448bbb190ac5
Tracy <tracymurenzi@gmail.com>
2021-05-12 10:03:24 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Data = () => {
     return (
-        <div className="MySideCale h-screen pt-20 fixed">
-        <h1 className="recorded text-2xl text-black">Recorded Values.</h1>
+        <div className=" h-screen pl-60 pt-20 fixed">
+        <h1 className="recorded text-2xl text-black">ENVIRONMENT DATA.</h1>
   
-        <ul className="sidebardata">
+        <ul className="pt-10 pl-10">
           <li className="flex  text-black text-lg">
             {" "}
             <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
