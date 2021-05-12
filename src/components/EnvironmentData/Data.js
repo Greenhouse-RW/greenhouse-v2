f4b3239b778da04c81fe5e0e4b3d1430508777f8
Tracy <tracymurenzi@gmail.com>
2021-05-12 09:59:37 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -2,8 +2,54 @@ import React from 'react'
 
 export const Data = () => {
     return (
-        <div>
-            <h1>Tracy is greeting you</h1>
-        </div>
+        <div className="MySideCale h-screen pt-20 fixed">
+        <h1 className="recorded text-2xl text-black">Recorded Values.</h1>
+  
+        <ul className="sidebardata">
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+      
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+  
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+  
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+  
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+  
+          <li className="flex  text-black text-lg">
+            {" "}
+            <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
+   <p>&#176;C</p>{" "}
+          </li>
+  
+          
+         
+        </ul>
+      </div>
     )
 }
