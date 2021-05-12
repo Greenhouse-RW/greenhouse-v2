fbd568dd99179e2fada2e75b985e2a27bbd31c16
Tracy <tracymurenzi@gmail.com>
2021-05-12 10:08:26 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const Data = () => {
     return (
         <div className=" h-screen pl-60 pt-20 fixed">
-        <h1 className="recorded text-2xl text-black">ENVIRONMENT DATA.</h1>
+        <h1 className=" text-2xl text-black font-bold">ENVIRONMENT DATA.</h1>
   
-        <ul className="pt-10 pl-10">
+        <ul className="pt-10 pl-6">
           <li className="flex  text-black text-lg">
             {" "}
             <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
