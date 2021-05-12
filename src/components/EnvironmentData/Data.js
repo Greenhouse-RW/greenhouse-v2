58e99e4acf50835070f5b118e50787cbedd68979
Tracy <tracymurenzi@gmail.com>
2021-05-12 10:10:34 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -5,7 +5,7 @@ export const Data = () => {
         <div className=" h-screen pl-60 pt-20 fixed">
         <h1 className=" text-2xl text-black font-bold">ENVIRONMENT DATA.</h1>
   
-        <ul className="pt-10">
+        <ul className="pt-10 pl-3">
           <li className="flex  text-black text-lg">
             {" "}
             <li className="pb-6">Temperature</li> <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3">34.5</li>{" "}
