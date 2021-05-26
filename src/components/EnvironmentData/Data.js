83a1bb33d30ca65bfc29025f9bd10e8796c9b0d4
kelliaUmuhire <kellumuhire@gmail.com>
2021-05-26 14:25:23 +0200

    facility control

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const Data = () => {
-    return (
-        <div>
-        <div className="  pl-64 pt-16 fixed overflow-auto">
-        <h1 className="text-xl text-black text-center font-bold pb-6">
+  return (
+    <div className="flex w-full space-x-10 flex-wrap">
+      <div className=" pt-16 mt-5 w-2/5">
+        <h1 className="text-xl text-black font-bold text-center pb-6">
           ENVIORNMENT DATA.
         </h1>
 
-        <ul className="flex flex-col justify-center items-center">
-
-        <li className="flex  text-black text-base">
+        <ul className="flex flex-col ml-24 mt-5 text-center">
+          <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Humidity</li>{" "}
             <li className="number1 ml-5 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
@@ -34,7 +33,6 @@ export const Data = () => {
             <p className="mt-2">&#176;C</p>{" "}
           </li>
 
-          
           <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Soil moisture</li>{" "}
@@ -43,7 +41,6 @@ export const Data = () => {
             <p className="mt-2">&#176;C</p>{" "}
           </li>
 
-
           <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Carbondioxide</li>{" "}
@@ -51,7 +48,7 @@ export const Data = () => {
             <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
             <p className="mt-2">&#176;C</p>{" "}
           </li>
-        
+
           <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Potential of H2</li>{" "}
@@ -60,7 +57,6 @@ export const Data = () => {
             <p className="mt-2">&#176;C</p>{" "}
           </li>
 
-
           <li className="flex  text-black text-base">
             {" "}
             <li className="pb-6 mt-2">Soil temperature</li>{" "}
@@ -68,11 +64,51 @@ export const Data = () => {
             <li className="number1 ml-2 mr-4 mb-6 pl-3 pr-3 py-2">34.5</li>{" "}
             <p className="mt-2">&#176;C</p>{" "}
           </li>
-        
         </ul>
       </div>
-
-
+      <div className="mt-5 w-2/5 text-center pt-16 mt-5 relative">
+        <div className="text-xl text-black font-bold text-center  pb-6 ">
+          Facility control
+        </div>
+        <div className="ml-10 flex flex-col mt-5 space-y-10">
+          <div className="flex space-x-10 relative">
+            <div className="mt-2">Heating Fan</div>
+            <div className="bg-dgreen shadow-sm flex justify-center align-center py-2.5 px-7 h-10 text-sm text-white absolute left-32">
+              ON
+            </div>
+            <div className="flex shadow-sm justify-center text-sm align-center py-2.5 px-7 h-10 absolute right-24">
+              OFF
+            </div>
+          </div>
+          <div className="flex space-x-10 relative">
+            <div className="mt-2">Cooling Fan</div>
+            <div className="bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+              ON
+            </div>
+            <div className="flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+              OFF
+            </div>
+          </div>
+          <div className="flex space-x-10 relative">
+            <div className="mt-2">Window</div>
+            <div className="bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+              ON
+            </div>
+            <div className="flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+              OFF
+            </div>
+          </div>
+          <div className="flex space-x-10 relative">
+            <div className="mt-2">Watering System</div>
+            <div className="bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+              ON
+            </div>
+            <div className="flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+              OFF
+            </div>
+          </div>
+        </div>
       </div>
-    )
-}
+    </div>
+  );
+};
