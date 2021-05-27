6cf7db8f1e716c92b419e5cc1c790ceae9557957
Tracy <tracymurenzi@gmail.com>
2021-05-27 12:59:43 +0200

    init

--- src/components/EnvironmentData/Data.js
+++ src/components/EnvironmentData/Data.js
@@ -142,48 +142,48 @@ export const Data = () => {
         <div className="ml-10 flex flex-col mt-5 space-y-12 relative">
           <div className="flex space-x-10 relative">
             <div className="mt-2">Heating Fan</div>
-            <button className="hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-lg flex justify-center align-center py-2.5 px-7 h-10 text-sm text-white absolute left-32">
+            <button className="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-lg flex justify-center align-center py-2.5 px-7 h-10 text-sm text-white absolute left-32">
               ON
             </button>
-            <button className="hover:bg-gray-800 rounded-md tracking-widest font-bold bg-gray-700 text-white flex shadow-lg justify-center text-sm align-center py-2.5 px-7 h-10 absolute right-24">
+            <button className="focus:outline-none hover:bg-gray-800 rounded-md tracking-widest font-bold bg-gray-700 text-white flex shadow-lg justify-center text-sm align-center py-2.5 px-7 h-10 absolute right-24">
               OFF
             </button>
           </div>
           <div className="flex space-x-10 relative">
             <div className="mt-2">Cooling Fan</div>
-            <div className="font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+            <button className="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
               ON
-            </div>
-            <div className="bg-gray-700 text-white font-bold flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+            </button>
+            <button className="focus:outline-none hover:bg-gray-800 rounded-md tracking-widest bg-gray-700 text-white font-bold flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
               OFF
-            </div>
+            </button>
           </div>
           <div className="flex space-x-10 relative">
             <div className="mt-2">Window</div>
-            <div className="font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+            <button className="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
               ON
-            </div>
-            <div className="font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+            </button>
+            <button className="focus:outline-none hover:bg-gray-800 rounded-md tracking-widest font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
               OFF
-            </div>
+            </button>
           </div>
           <div className="flex space-x-10 relative">
             <div className="mt-2">Watering System</div>
-            <div className="font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+            <button className="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
               ON
-            </div>
-            <div className="font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+            </button>
+            <button className="focus:outline-none hover:bg-gray-800 rounded-md tracking-widest font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
               OFF
-            </div>
+            </button>
           </div>
-          <div className="flex space-x-10 relative">
+          <div className="led flex space-x-10 relative">
             <div className="mt-2">LED Lamp</div>
-            <div className="font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
+            <button className="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-dgreen shadow-sm flex justify-center align-center py-2.5 text-sm px-7 h-10 text-white absolute left-32">
               ON
-            </div>
-            <div className="font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
+            </button>
+            <button className="focus:outline-none hover:bg-gray-800 rounded-md tracking-widest font-bold bg-gray-700 text-white flex shadow-sm justify-center align-center py-2.5 text-sm px-7 h-10 absolute right-24">
               OFF
-            </div>
+            </button>
           </div>
       
         </div>
@@ -191,3 +191,4 @@ export const Data = () => {
     </div>
   );
 };
+
