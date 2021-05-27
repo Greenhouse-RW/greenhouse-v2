e9e6b7953bd4140dd030bb1e1e5782454abaefa7
pauline12ish34 <paulineishimwe12@gmail.com>
2021-05-27 18:55:45 +0200

    changes on conditions

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -12,29 +12,29 @@ function Condition (){
 <p><label class="w-10 mr-5">Illumination:</label> <input class="w-16 border-2  border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> <Button/></p>
  <p><label class="w-10 mr-20">CO2 :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/><Button/></p>
 <p><label class="w-10 mr-5">Soil moisture  :</label><input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/><Button/> </p>   
-{/*<p><label class="w-20 mr-5">PH :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> <Button/> </p> 
-<p><label class="w-20 mr-5">EC :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/><Button/> </p>  */}
+<p><label class="w-10 mr-20">PH :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> <Button/> </p> 
+<p><label class="w-10 mr-20">EC :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/><Button/> </p> 
 </div>
 
 
 <div class="ml-2" >
-{/* <p>Temperature: <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/>  C </p>
-<p>Illumination: <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> bx</p>
-<p>CO2 : <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-2 border-2 border-black-500"/> %</p><br/><br/>   
-<p>Soil moisture  :<input class="border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> % </p> <br/><br/>  
-<p>PH : <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> </p> <br/><br/>
-<p>EC : <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> m/s </p>  */}
+<p><label>Temperature:</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/>  C </p>
+<p><label>Illumination:</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> bx</p>
+<p><label>CO2 :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> %</p><br/><br/>   
+<p><label>Soil moisture  :</label><input class="border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> % </p> <br/><br/>  
+<p><label>PH :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> </p> <br/><br/>
+<p><label>EC :</label> <input class="shadow-sm w-16 border-2 border-black-500"/> - <input class="shadow-sm w-16 border-2 border-black-500"/> m/s </p> 
 
 </div>
 </div>
-<div>
+<div class ="mt-5 mx-64">
 <form action="" method="post">
-<h4>Enter your credentials for sudden information.</h4>
-<label for="lname">Last Name :</label><input class="w-23 border-2 border-black-500"/> <br/><br/>
-<label for="fname">First Name :</label><input class="w-23 border-2 border-black-500"/><br/><br/>
-<label for="phone">Telephone No:</label><input class="w-23 border-2 border-black-500"/><br/><br/>
-<label for="mail">Email:</label> <input class="w-23 border-2 border-black-500"/><br/><br/><br/>
-<input type="button" value="Submit" className="w-23 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"/>
+<h4 class="my-8">Enter your credentials for sudden information.</h4>
+<input class="w-80 border-2 rounded-xl p-1 px-2" placeholder="Enter your Last Name "/> <br/><br/>
+<input class="w-80 border-2 rounded-xl p-1 px-2" placeholder="Enter your Last Name "/><br/><br/>
+<input class="w-80 border-2  rounded-xl p-1 px-2" placeholder="Enter your phone number"/><br/><br/>
+<input class="w-80 border-2  rounded-xl p-1 px-2" placeholder="Enter your email "/><br/><br/><br/>
+<input type="button" value="Submit" className="mx-24 rounded-xl w-44 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 "/>
 
 </form>
     
