7958d8bf8632710e6786d457bbc8d913da54e83f
Tracy <tracymurenzi@gmail.com>
2021-05-31 13:08:01 +0200

    init

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -3,7 +3,34 @@ import React from 'react'
 const Support = () => {
     return (
         <div>
-            tracy
+            <form>
+            <div class="field">
+              <label for="">Full Name</label>
+              <input class="full_name"
+                     type="text"
+                     name="full-name"
+                     value="" />
+            </div>
+            <div class="field">
+              <label for="">Email</label>
+              <input class="full_name"
+                     type="text"
+                     name="full-name" />
+            </div>
+            <div class="field">
+              <label for="">Password</label>
+              <input class="full_name"
+                     type="text"
+                     name="full-name" />
+            </div>
+            <div class="field">
+              <label for="">Birthday</label>
+              <input class="full_name"
+                     type="text"
+                     name="full-name" />
+            </div>
+            </form>
+            
         </div>
     )
 }
