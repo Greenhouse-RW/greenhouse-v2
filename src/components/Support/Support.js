bf58330ea395c499bd658a16a7ac46ca27a42835
Tracy <tracymurenzi@gmail.com>
2021-05-31 20:53:07 +0200

    init

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -2,10 +2,34 @@ import React from 'react'
 import './support.css';
 import Faq from 'react-faq-component';
 
-const Support = () => {
-    return (
-      <div>Tracy</div>
-    )
+const data = {
+  title: "FAQ (How it works)",
+  rows: [
+    {
+      title: "Lorem ipsum dolor sit amet,",
+      content: "Lorem ipsum dolor sit amet, consectetur "
+    },
+    {
+      title: "Nunc maximus, magna at ultricies elementum",
+      content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+    },
+    {
+      title: "Curabitur laoreet, mauris vel blandit fringilla",
+      content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+    },
+    {
+      title: "What is the package version",
+      content: "v1.0.5"
+    }]
 }
 
-export default Support
+export default class Support extends Component {
+  render() {
+    return (
+      <div>
+        <Faq data={data}/>
+      </div>
+    )
+  }
+}
+
