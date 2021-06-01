bae474d5b7bcd910b6fe09a402403c51827dc0f7
Tracy <tracymurenzi@gmail.com>
2021-06-01 10:38:15 +0200

    init

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Button from './Button.js'
+import './Condition.css'
 
 function Condition() {
 
     return (
         <div>
       <div className="flex w-full space-x-10 justify-center">
-      <div className=" pt-16 mt-5 w-2/5">
+      <div className=" mt-16 w-2/5">
              {/* for row */}
-        <div class = "ml-3" > SET THE VALUE OF PLANT’ S BETTER HEALTH </div>
+        <div class = "text-xl text-black font-bold text-center" > SET THE VALUE OF PLANT’ S BETTER HEALTH. </div>
 
         <div class = " flex flex-col space-y-6  relative" >
         
-        <div className = "flex space-x-6 relative mt-24" >
+        <div className = "flex space-x-6 relative mt-16" >
                 <div className = "w-12 mt-2 mr-3" > Temperature: </div>
                 <div> <input class="h-8 ml-5 shadow flex w-16 mt-2"/></div>
                 <div>-</div>
@@ -20,7 +21,7 @@ function Condition() {
                 <div><Button/></div>
         
             </div>
-            <div className = "flex space-x-6 relative mt-24" >
+            <div className = "flex space-x-6 relative" >
               <div className = "w-12 mt-2 mr-3" > Illumination: </div>
               <div><input class="h-8 ml-5 shadow flex w-16 mt-2  "/> </div>
               <div>-</div>
