24788e29864410c4c15af99734f8eec0b1df3f18
Tracy <tracymurenzi@gmail.com>
2021-06-01 11:17:07 +0200

    init

--- src/components/Env_conditions/Button.js
+++ src/components/Env_conditions/Button.js
@@ -5,7 +5,7 @@ class Button extends Component {
     render(){
 
 return (
-<button class="focus:outline-none shadow-lg w-20 h-10 bg-dgreen hover:bg-green-700  text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow justify-center"> 
+<button class="focus:outline-none shadow-lg w-20 h-10 bg-dgreen hover:bg-green-700  text-white font-bold py-2 px-4 border border-gray-400 rounded shadow justify-center"> 
 {/* <button class="focus:outline-none hover:bg-green-700  rounded-md tracking-widest font-bold bg-white shadow-lg flex justify-center align-center py-2.5 px-7 h-10 text-sm absolute left-32" > */}
 
 Button
