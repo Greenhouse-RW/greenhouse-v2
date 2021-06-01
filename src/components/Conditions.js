6c96079c011873ef16e341b531803f8abc59d36b
Tracy <tracymurenzi@gmail.com>
2021-06-01 10:40:19 +0200

    init

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -29,7 +29,7 @@ function Condition() {
               <div><Button/></div>
         
                </div>
-               <div className = "flex space-x-6 relative mt-24" >
+               <div className = "flex space-x-6 relative" >
              <div className = "w-12 mt-2 mr-3" > CO2 : </div>
              <div><input class="h-8 ml-5 shadow flex w-16 mt-2  "/> </div>
              <div>-</div>
@@ -37,7 +37,7 @@ function Condition() {
              <div> <Button/></div>
         
                </div>
-               <div className = "flex space-x-6 relative mt-24" >
+               <div className = "flex space-x-6 relative " >
              <div className = "w-17 mt-2 -m-3" > Soil moisture : </div>
              <div><input class="h-8 shadow flex w-16 mt-2"/> </div>
              <div>-</div>
@@ -45,7 +45,7 @@ function Condition() {
              <div><Button/> </div> 
         
                </div>
-               <div className = "flex space-x-6 relative mt-24" >
+               <div className = "flex space-x-6 relative" >
              <div className = "w-12 mt-2 mr-3" > PH : </div>
              <div><input class="h-8 ml-5 shadow flex w-16 mt-2 "/> </div>
              <div>-</div>
@@ -54,7 +54,7 @@ function Condition() {
         
                </div>
 
-             <div className = "flex space-x-6 relative mt-24" >
+             <div className = "flex space-x-6 relative" >
              <div className = "mr-3 w-12 mt-2 " > EC : </div>
              <div > <input className="h-8 ml-5 shadow flex w-16 mt-2 "/> </div>
              <div>-</div>
@@ -69,7 +69,7 @@ function Condition() {
 </div>
 </div>
         
-<div className="mt-5 pt-16 w2/5 text-center relative">
+<div className="mt-16 w2/5 text-center relative">
 
         <div class = " flex flex-col space-y-6  relative" >
             <div className = "flex space-x-6 relative mt-24" >
@@ -127,19 +127,6 @@ function Condition() {
         
          </div>
          </div>
-         <div class ="mt-5 ml-96">
-<form action="" method="post">
-<h4 class="my-8">Enter your credentials for sudden information.</h4>
-<input class="w-80 h-18 border-2 rounded-xl p-1 px-2" placeholder="Enter your Last Name "/> <br/><br/>
-<input class="w-80 h-18 border-2 rounded-xl p-1 px-2" placeholder="Enter your Last Name "/><br/><br/>
-<input class="w-80 h-18 border-2  rounded-xl p-1 px-2" placeholder="Enter your phone number"/><br/><br/>
-<input class="w-80 h-18 border-2  rounded-xl p-1 px-2" placeholder="Enter your email "/><br/><br/><br/>
-<input type="button" value="Submit" className=" mb-12 mx-24 rounded-xl w-44 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 "/>
-
-</form>
-
-    </div>
-
 
          </div>
     )
