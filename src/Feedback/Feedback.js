2745f1e0e5c83e3416f1960b921cf3c548cfffaf
Tracy <tracymurenzi@gmail.com>
2021-06-17 17:44:18 +0200

    init

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -20,26 +20,26 @@ export default function feedback() {
   
 
     <div className="svg-container">
-    <ul class="slides">
+    <ul className="slides">
 <input type="radio" name="radio-btn" id="img-1" checked />
-<li class="slide-container">
-<div class="slide">
-    <img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
+<li className="slide-container">
+<div className="slide">
+    <img className="h-screen" src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
 </div>
-<div class="nav">
+<div className="nav">
     <label for="img-6" class="prev">&#x2039;</label>
     <label for="img-2" class="next">&#x203a;</label>
 </div>
 </li>
 
 <input type="radio" name="radio-btn" id="img-2" />
-<li class="slide-container">
-<div class="slide">
-  <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
+<li className="slide-container">
+<div className="slide">
+  <img className="h-screen" src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
 </div>
-<div class="nav">
-    <label for="img-1" class="prev">&#x2039;</label>
-    <label for="img-3" class="next">&#x203a;</label>
+<div className="nav">
+    <label for="img-1" className="prev">&#x2039;</label>
+    <label for="img-3" className="next">&#x203a;</label>
 </div>
 </li>
 
