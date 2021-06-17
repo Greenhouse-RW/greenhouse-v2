072732bb6117a6c292685f45b3a2025d2ae23526
Tracy <tracymurenzi@gmail.com>
2021-06-17 18:18:19 +0200

    init

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,9 +5,9 @@ const LandingPage = () => {
     return ( 
         <div>
 
-        <div className="flex justify-between">
+        <div className="flex h-screen overflow-x-hidden overflow-y-hidden">
             
-            <div className="more ml-40">
+            <div className="w-1/2 grid justify-items-center">
              <h1 className="text-6xl font-bold tracking-wider mt-24 text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-600">WELCOME TO <br></br>AUTO-GREEN</h1>
              <p className="explain text-lg leading-relaxed mt-8 tracking-wide">AUTO-GREEN is an automated greenhouse that <br></br>  involves the automatic monitoring and controlling of <br></br> climatic  parameters which directly or indirectly govern <br></br> the plant growth and increase the productivity.</p>
             <p className="explain text-lg leading-relaxed tracking-wide">Enjoy watching your plant grow healthy and productive <br></br> in the simplest way by interacting with our application.</p>
@@ -24,13 +24,13 @@ const LandingPage = () => {
 
             </div>
             </div>
-            <div className="svg-container">
-            <ul class="slides">
+            <div className="w-1/2 h-screen">
+            <ul class="slides h-screen">
     <input type="radio" name="radio-btn" id="img-1" checked />
     <li class="slide-container">
-		<div class="slide">
-			<img src="https://images.unsplash.com/photo-1566208541413-b8575e37587d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTgyfHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
-        </div>
+		<div class="slide h-screen">
+			<img src="https://images.unsplash.com/photo-1566208541413-b8575e37587d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTgyfHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="h-screen w-full" />
+    </div>
 		<div class="nav">
 			<label for="img-6" class="prev">&#x2039;</label>
 			<label for="img-2" class="next">&#x203a;</label>
@@ -40,7 +40,7 @@ const LandingPage = () => {
     <input type="radio" name="radio-btn" id="img-2" />
     <li class="slide-container">
         <div class="slide">
-          <img src="https://images.unsplash.com/photo-1563865973424-86ee636944c1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTgyfHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          <img src="https://images.unsplash.com/photo-1563865973424-86ee636944c1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTgyfHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="w-full"/>
         </div>
 		<div class="nav">
 			<label for="img-1" class="prev">&#x2039;</label>
@@ -51,7 +51,7 @@ const LandingPage = () => {
     <input type="radio" name="radio-btn" id="img-6" />
     <li class="slide-container">
         <div class="slide">
-          <img src="https://images.unsplash.com/photo-1533322069166-aa4d608459df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM3fHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          <img src="https://images.unsplash.com/photo-1533322069166-aa4d608459df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM3fHxncmVlbmhvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="w-full" />
         </div>
 		<div class="nav">
 			<label for="img-5" class="prev">&#x2039;</label>
