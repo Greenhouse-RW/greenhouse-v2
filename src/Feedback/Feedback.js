246929fb14c168bd80ff09f3d28b8bfb22aa8a14
Tracy <tracymurenzi@gmail.com>
2021-06-17 12:05:17 +0200

    init

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import './feedback.css';
+import './feedback.css'
 
 export default function feedback() {
     return (
         <div>
-            <form>      
-  <input name="name" type="text" class="feedback-input" placeholder="Name" />   
-  <input name="email" type="text" class="feedback-input" placeholder="Email" />
-  <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
-  <input type="submit" value="SUBMIT"/>
-</form>
+            
         </div>
+        
     )
 }
