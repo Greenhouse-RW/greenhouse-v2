7f4707e390f4cdb9bfa8a9df1a45a0c086ee001c
Tracy <tracymurenzi@gmail.com>
2021-06-17 17:35:32 +0200

    init

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -9,7 +9,8 @@ export default function feedback() {
 <div className="flex justify-between">
 
 <div>
-    <form className="feedback-form">      
+    <form className="feedback-form">  
+    <h1 className="grid justify-items-center text-base mb-8 font-bold ">Share With Us Your Feedback.</h1>    
   <input name="name" type="text" class="feedback-input" placeholder="Name" />   
   <input name="email" type="text" class="feedback-input" placeholder="Email" />
   <textarea name="text" class="feedback-input" placeholder="Feedback"></textarea>
@@ -42,54 +43,8 @@ export default function feedback() {
 </div>
 </li>
 
-<input type="radio" name="radio-btn" id="img-3" />
-<li class="slide-container">
-<div class="slide">
-  <img src="http://farm9.staticflickr.com/8068/8250438572_d1a5917072_z.jpg" />
-</div>
-<div class="nav">
-    <label for="img-2" class="prev">&#x2039;</label>
-    <label for="img-4" class="next">&#x203a;</label>
-</div>
-</li>
-
-<input type="radio" name="radio-btn" id="img-4" />
-<li class="slide-container">
-<div class="slide">
-  <img src="http://farm9.staticflickr.com/8061/8237246833_54d8fa37f0_z.jpg" />
-</div>
-<div class="nav">
-    <label for="img-3" class="prev">&#x2039;</label>
-    <label for="img-5" class="next">&#x203a;</label>
-</div>
-</li>
-
-<input type="radio" name="radio-btn" id="img-5" />
-<li class="slide-container">
-<div class="slide">
-  <img src="http://farm9.staticflickr.com/8055/8098750623_66292a35c0_z.jpg" />
-</div>
-<div class="nav">
-    <label for="img-4" class="prev">&#x2039;</label>
-    <label for="img-6" class="next">&#x203a;</label>
-</div>
-</li>
-
-<input type="radio" name="radio-btn" id="img-6" />
-<li class="slide-container">
-<div class="slide">
-  <img src="http://farm9.staticflickr.com/8195/8098750703_797e102da2_z.jpg" />
-</div>
-<div class="nav">
-    <label for="img-5" class="prev">&#x2039;</label>
-    <label for="img-1" class="next">&#x203a;</label>
-</div>
-</li>
-
 </ul>
-    </div>
-
-  
+</div>
 </div>
 </div>
         </div>
